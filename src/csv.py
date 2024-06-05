341a74d7cbf8d577e4ae4c4afa31282d85f1eea8
Juan Helou <juanseh7@hotmail.com>
2024-06-05 18:07:28 -0300

    Lets csv have only one V column

--- src/csv.py
+++ src/csv.py
@@ -17,31 +17,17 @@ def read_csv(filename, vinPort, voutPort):
 
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile)
-        if len(reader.fieldnames) == 5:
-            if vinPort > 4 or voutPort > 4:
-                print("Port number exceeds the number of ports")
-                return None
-            if vinPort == 0:
-                vinPortString = "3"
-            if voutPort == 0:
-                voutPortString = "1"
-            for row in reader:
-                if row["x-axis"] != "second":
-                    data["t"].append(float(row["x-axis"])*1e6 ) # Convert to microseconds
-                    data["vout"].append(float(row[voutPortString]) )
-                    data["vin"].append(float(row[vinPortString]) )
-        else:
-            if vinPort > 2 or voutPort > 2:
-                print("Port number exceeds the number of ports")
-                return None
-            if vinPort == 0:
-                vinPortString = "2"
-            if voutPort == 0:
-                voutPortString = "1"
-            for row in reader:
-                if row["x-axis"] != "second":
-                    data["t"].append(float(row["x-axis"])*1e6 ) # Convert to microseconds
-                    data["vout"].append(float(row[voutPortString]) )
+        cols = len(reader.fieldnames)
+
+        if vinPort >= cols or voutPort >= cols:
+            print("Port number exceeds the number of ports")
+            return None
+        for row in reader:
+            if row["x-axis"] != "second":
+                data["t"].append(float(row["x-axis"])*1e6 ) # Convert to microseconds
+                if voutPort != 0:
+                    data["vout"].append(float(row[voutPortString]))
+                if vinPort != 0:
                     data["vin"].append(float(row[vinPortString]) )
 
     return data
@@ -66,17 +52,14 @@ def parse_csv(filename, offset, vinPort, voutPort):
 
     if data is not None:
         # Apply offset to the data
-        vin_offset = [v + offset for v in data["vin"]]
-        vout_offset = [v + offset for v in data["vout"]]
-
-        # Create a plot
-        #plt.figure(figsize=(10, 6))
-
-        # Plot vin
-        plt.plot(data["t"], vin_offset, label='Vin', marker='o', color='red')
-
-        # Plot vout
-        plt.plot(data["t"], vout_offset, label='Vout', marker='x', color='blue')
+        if data["vin"].__len__() > 0:
+            vin_offset = [v + offset for v in data["vin"]]
+            # Plot vin
+            plt.plot(data["t"], vin_offset, label='Vin', marker='o', color='red')
+        if data["vout"].__len__() > 0:
+            vout_offset = [v + offset for v in data["vout"]]
+            # Plot vout
+            plt.plot(data["t"], vout_offset, label='Vout', marker='x', color='blue')
 
         # Adding titles and labels
         plt.title('Vin and Vout over Time')
